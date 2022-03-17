9ad13e3da2054283b38d7be0b824b635cbdbd5c4
Annie <Anniebelkin97@gmail.com>
2022-03-17 21:39:00 +0200

    refactor: move selectors & methods to DOM variable
    The following changes has been made:
    1. Most of ths selectors were moved to a DOM variable.
    2. Create nutrients array for easier iteration over data.
    3. Rewrite all the functions that update the nutrient values table to DOM
       variable as three methods: updatePercent, updateTotal and updateTable.
    4. Add max value and step to inputs in the nutrient values table.
    5. Remove the addNewIngredient function. Will be added later on.

--- script.js
+++ script.js
@@ -1,92 +1,70 @@
 (() => {
 
+    const nutrients = ["cal", "protein", "carbs", "fat"];
+
     const state = {
         ingredientsList: [],
         counter: 0
     };
 
-    const modal = $("form");
-    const fieldset = modal.children("fieldset")
-    const table = fieldset.children("table");
-    const form = table.find("input");
-    const ingredientName = fieldset.children("#i-name");
+    const DOM = {
+        form: {
+            modal: $("form"),
+            inputs: {
+                updateTotal: function(type, value, amount){
+                    const ans = (value / 100) * amount;
+                    console.log(type);
+                    this[type].val(parseFloat(ans).toFixed(2));
+                },
+                updatePercent: function(type, value, amount){
+                    const ans = (value / amount) * 100;
+                    this[type + "-100"].val(parseFloat(ans).toFixed(2));
+                },
+                updateTable: function(target){
+                    if (target.name === "amount"){
+                        for (type of nutrients){
+                            this.updateTotal(type, this[type + "-100"].val(), target.value);
+                        }
+                    } 
+                    else if (target.required){
+                        this.updatePercent(target.name, target.value, this.amount.val());
+                    }
+                    else {
+                        this.updateTotal(target.name.slice(0,-4), target.value, this.amount.val())
+                    }
+                }
+            }
+        }
+    }
 
-    const currInputs = {};
-    for (i in form) {
-        if (form[i].required){
-            currInputs[form[i].name] = $(form[i]);
-        };
-    };
+    DOM.form.fieldset = DOM.form.modal.children("fieldset")
+    DOM.form.fieldset.find("input").each( function(){
+        if (this.name != "") {
+            DOM.form.inputs[this.name] = $(this);
+        }
+    });
 
     const openModal = () => {
-        modal.removeClass("hidden");
-        ingredientName.focus();
+        DOM.form.modal.removeClass("hidden");
+        DOM.form.inputs["name"].focus();
     };
 
     const closeModal = () => {
-        modal.addClass("hidden");
-        modal[0].reset();
+        DOM.form.modal.addClass("hidden");
+        DOM.form.modal[0].reset();
     };
-
-    const addNewIngredient = () => {
-        const newIngredient = {
-            id: state.counter,
-            name: ingredientName.val()
-        };
-        for (i in currInputs){
-            if (currInputs[i][0].required){
-                newIngredient[i] = currInputs[i].val();
-            }
-        }
-        state.ingredientsList.push(newIngredient);
-        state.counter++;
-        console.log(state);
-    }
-
-    const calcPercent = (part, total) => Math.round((part / total) * 100);
-
-    const calcPartofTotal = (percent = 0, total) => Math.round((percent / 100) * total);
-
-    const getPrevCell = (target) => target.parent().prev().children().first();
-
-    const getNextCell = (target) => target.parent().next().children().first();
-
-    const updatePrev = (target, total) => {
-        getPrevCell(target).val(calcPercent(target.val(), total));
-    }
-
-    const updateNext = (target, total) => {
-        getNextCell(target).val(calcPartofTotal(target.val(), total));
-    }
-
-    const updateTable = (total) => {
-        const resCal = calcPartofTotal(getPrevCell(currInputs.cal).val(), total),
-              resProtein = calcPartofTotal(getPrevCell(currInputs.protein).val(), total),
-              resCarbs = calcPartofTotal(getPrevCell(currInputs.carbs).val(), total),
-              resFat = calcPartofTotal(getPrevCell(currInputs.fat).val(), total);
-        currInputs.cal.val(resCal);
-        currInputs.protein.val(resProtein);
-        currInputs.carbs.val(resCarbs);
-        currInputs.fat.val(resFat);
-    }
 
     $("#add-new-btn").click(openModal);
 
-    table.change((e) => {
-        console.log("event");
-        const target = e.target;
-        const amount = currInputs.amount.val();
-        if (target.name === "amount") updateTable(amount);
-        else if (target.required) updatePrev($(target), amount);
-        else updateNext($(target), amount);
+    DOM.form.fieldset.children("table").change((e) => {
+        DOM.form.inputs.updateTable(e.target);
     });
 
-    modal.submit(() => {
+    DOM.form.modal.submit(() => {
         addNewIngredient();
         closeModal();
     });
 
-    fieldset.children("#cancel-btn").click(closeModal);
-
+    DOM.form.inputs.cancel.click(closeModal);
 
 })()
