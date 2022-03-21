29b0f86300197bd892298ac686b8f3b446ba84fd
Annie <Anniebelkin97@gmail.com>
2022-03-21 11:03:23 +0200

    feat: make ingredients list dynamic
    The following changes has been made:
    1. Move the add new ingredient button higher.
    2. Add list selector to DOM variable.
    4. Add click event listener on list parent.
    5. Add update to state variable, remove list and counter.
    6. Move openModal and closeModal functions inside DOM variable.
    7. The add, edit and delete button are working.
    8. Remove max value from total nutrient value inputs.
    9. Fix spacing on list items.
    10. Add nissing semi-colons.

--- script.js
+++ script.js
@@ -3,17 +3,30 @@
     const nutrients = ["cal", "protein", "carbs", "fat"];
 
     const state = {
-        ingredientsList: [],
-        counter: 0
+        update: {
+            isActive: false,
+            pointer: {},
+            data: {},
+            init: function(item){
+                this.isActive = true;
+                this.pointer = item;
+                this.data = JSON.parse(item.attr("data-json"));
+            },
+            resolve: function(){
+                this.isActive = false;
+                this.pointer = {};
+                this.data = {};
+            }
+        }
     };
 
     const DOM = {
+        ingredientsList: $("#ingredients-list"),
         form: {
             modal: $("form"),
             inputs: {
                 updateTotal: function(type, value, amount){
                     const ans = (value / 100) * amount;
-                    console.log(type);
                     this[type].val(parseFloat(ans).toFixed(2));
                 },
                 updatePercent: function(type, value, amount){
@@ -33,9 +46,26 @@
                         this.updateTotal(target.name.slice(0,-4), target.value, this.amount.val())
                     }
                 }
+            },
+            open: function(){
+                this.modal.removeClass("hidden");
+                this.inputs.name.focus();
+            },
+            close: function(){
+                this.modal.addClass("hidden");
+                this.modal.trigger("reset");
+            },
+            editItem: function(item){
+                this.inputs.name.val(item.name);
+                this.inputs.amount.val(item.amount);
+                for (type of nutrients){
+                    this.inputs[type].val(item[type]);
+                    this.inputs[type].trigger("change");
+                }
+                this.open();
             }
         }
-    }
+    };
 
     DOM.form.fieldset = DOM.form.modal.children("fieldset")
     DOM.form.fieldset.find("input").each( function(){
@@ -44,27 +74,68 @@
         }
     });
 
-    const openModal = () => {
-        DOM.form.modal.removeClass("hidden");
-        DOM.form.inputs["name"].focus();
+    const createIngredientListItem = (ingredient) => {
+        const { name, amount, cal, protein, carbs, fat } = ingredient;
+        return `
+            <li class="ingredient flexed frame" title="${name}" 
+                data-json='${JSON.stringify(ingredient)}'>
+                <header> 
+                    ${name} (${amount}<sub>g</sub>)<hr/>
+                    <small>
+                        Cal: ${cal} | 
+                        Protein: ${protein} | 
+                        Carbs: ${carbs} | 
+                        Fat: ${fat}
+                    </small>
+                </header>
+                <nav>
+                    <i title="edit"></i>
+                    <i title="delete"></i>
+                </nav>
+            </li>`
     };
 
-    const closeModal = () => {
-        DOM.form.modal.addClass("hidden");
-        DOM.form.modal[0].reset();
+    const getData = () => {
+        const data = {
+            name: DOM.form.inputs.name.val(),
+            amount: DOM.form.inputs.amount.val()
+        }
+        for (type of nutrients) data[type] = DOM.form.inputs[type].val();
+        return data;
     };
 
-    $("#add-new-btn").click(openModal);
+    DOM.ingredientsList.parent().click((e) => {
+        const target = e.target;
+        switch (target.title) {
+            case "add":
+                DOM.form.open();
+                break;
+            case "edit":
+                state.update.init($(target).closest("li"));
+                DOM.form.editItem(state.update.data);
+                break;
+            case "delete":
+                $(target).closest("li").remove();
+                break;  
+        }
+    });
 
     DOM.form.fieldset.children("table").change((e) => {
         DOM.form.inputs.updateTable(e.target);
     });
 
     DOM.form.modal.submit(() => {
-        addNewIngredient();
-        closeModal();
+        const data = getData();
+        const listItem = createIngredientListItem(data);
+        if (state.update.isActive){
+            state.update.pointer.replaceWith(listItem);
+            state.update.resolve();
+        } else {
+            DOM.ingredientsList.append(listItem);
+        }
+        DOM.form.close();
     });
 
-    DOM.form.inputs.cancel.click(closeModal);
+    DOM.form.inputs.cancel.click(() => DOM.form.close());
 
-})()
+})();
